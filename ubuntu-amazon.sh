f26428f2ffa48abbcdf8b003b9f6a3a9566480d2
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-03-23 21:33:26 +0530

    Create ubuntu-amazon.sh

---
+++ ubuntu-amazon.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+apt update
+apt install apache2 git -y
+systemctl start apache2
+systemctl status apache2
+cd /var/www/html
+git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
+mv online-clone-amazon/* .
+tail -f /var/log/apache2/access.log
